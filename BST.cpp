011f67ff8c8c0e7457b8fcb940a3f63760bfb36c
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2023-12-03 22:02:34 +0600

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -3,114 +3,92 @@ using namespace std;
 
 struct Node {
     int data;
-    Node *ladd;
-    Node *radd;
+    Node *left;
+    Node *right;
 };
 
 Node *root = NULL;
 
-
-void create (Node *r , Node *new1) {
+void create (Node *r, Node *new1) {
     if (new1->data < r->data) {
-        if (r->ladd == NULL) {
-            r->ladd = new1;
+        if (r->left == NULL) {
+            r->left = new1;
         }
         else {
-            create(r->ladd, new1);
+            create(r->left, new1);
         }
     }
-    if (new1->data > r->data) {
-        if (r->radd == NULL) {
-            r->radd = new1;
+    else if (new1->data > r->data) {
+        if (r->right == NULL) {
+            r->right = new1;
         }
         else {
-            create(r->radd, new1);
+            create(r->right, new1);
         }
     }
-    if (new1->data == r->data) {
-        cout << "Duplicate element not allower" << endl;
+    else {
+        cout << "Duplicate" << endl;
     }
 }
-
-void inorder (Node *r){
-    if (r != NULL) {
-        inorder(r->ladd);
-        cout << r->data << " ";
-        inorder(r->radd);
+void inorder (struct Node *root) {
+    if (root == NULL) {
+        return;
     }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
 }
 
-void preorder (Node *r){
-    if (r != NULL) {
-        cout << r->data << " ";
-        preorder(r->ladd);
-        preorder(r->radd);
+void preorder (struct Node *root) {
+    if (root == NULL) {
+        return;
     }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
 }
 
-void postorder (Node *r){
-    if (r != NULL) {
-        postorder(r->ladd);
-        postorder(r->radd);
-        cout << r->data << " ";
+void postorder (struct Node *root) {
+    if (root == NULL) {
+        return;
     }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
 }
 
-signed main () {
-    int ch;
-    char choice;
-    do {
-        cout << endl << "1. create" << endl;
-        cout << "2. inorder" << endl;
-        cout << "3. preorder" << endl;
-        cout << "4. postorder" << endl;
-        cout << "5. exit" << endl;
-        cout << "enter your choice : ";
-        cin >> ch;
 
-        switch (ch) {
-            case 1 :
-                do {
-                    cout << endl;
-                    int n;
-                    cout << "enter an element : ";
-                    cin >> n;
+int main () {
+    cout << "Enter the size of nodes : ";
+    int number;
+    cin >> number;
 
-                    Node *new1 = new Node();
-                    new1->ladd = new1->radd=NULL;
-                    new1->data = n;
+    while (number--) {
+        int n;
+        cin >> n;
 
-                    if (root == NULL) {
-                        root = new1;
-                    }
-                    else {
-                        create (root, new1);
-                    }
-                    cout << "Want to continue : " ;
-                    cin >> choice;
-                }while(choice == 'y' || choice == 'Y');break;
+        Node *new1 = new Node();
+        new1->left = NULL;
+        new1->right = NULL;
+        new1->data = n;
 
-            case 2 :
-                cout << endl;
-                inorder(root);
-                cout << endl;
-                break;
 
-            case 3 :
-                cout << endl;
-                preorder(root);
-                cout << endl;
-                break;
-
-            case 4 :
-                cout << endl;
-                postorder(root);
-                cout << endl;
-                break;
-
+        if (root == NULL) {
+            root = new1;
         }
-    }while(ch != 5);
+        else {
+            create(root, new1);
+        }
+    }
 
+    inorder(root);
+    cout << endl;
+
+    preorder(root);
+    cout << endl;
+
+    postorder(root);
+    cout << endl;
 
     return 0;
 }
