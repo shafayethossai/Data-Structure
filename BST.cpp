7113dae8cbe1c1c82d1295c12dda038b501acadd
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-04-15 18:05:44 +0600

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -56,6 +56,70 @@ void postorder (struct Node *root) {
     postorder(root->right);
     cout << root->data << " ";
 }
+Node* search(Node* root, int key) {
+    if (root == NULL || root->data == key) {
+        return root;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    }
+
+    return search(root->right, key);
+}
+
+Node* findMin(Node* root) {
+    if (root == NULL) {
+        return NULL;  // Tree is empty
+    }
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* findMax(Node* root) {
+        if (root == NULL) {
+            return NULL;  // Tree is empty
+        }
+        while (root->right != NULL) {
+            root = root->right;
+        }
+        return root;
+    }
+
+Node* findSuccessor(Node* root, int key) {
+    Node* current = root;
+    Node* successor = NULL;
+
+    while (current != NULL) {
+        if (current->data > key) {
+            successor = current;
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    return successor;
+}
+
+Node* findPredecessor(Node* root, int key) {
+    Node* current = root;
+    Node* predecessor = NULL;
+
+    while (current != NULL) {
+        if (current->data < key) {
+            predecessor = current;
+            current = current->right;
+        } else {
+            current = current->left;
+        }
+    }
+
+    return predecessor;
+}
+
 
 
 int main () {
@@ -81,14 +145,75 @@ int main () {
         }
     }
 
+    cout << "In order "; // In order traverse
     inorder(root);
     cout << endl;
 
+    cout << "Pre order : ";  // Pre order traverse
     preorder(root);
     cout << endl;
 
+    cout << "Post order : "; // Post order traverse
     postorder(root);
     cout << endl;
+
+    
+    int key;
+    cout << "Enter the value to search: "; // Searching any value
+    cin >> key;
+
+    Node* result = search(root, key);
+    if (result != NULL) {
+        cout << "Value found in the tree" << endl;
+    }
+    else {
+        cout << "Value not found in the tree" << endl;
+    }
+
+    
+    Node* minNode = findMin(root); // Finding Minimum value
+    if (minNode != NULL) {
+        cout << "Minimum value in the tree: " << minNode->data << endl;
+    }
+    else {
+        cout << "Tree is empty" << endl;
+    }
+
+    
+    Node* maxNode = findMax(root); // Finding Maximum value
+    if (maxNode != NULL) {
+        cout << "Maximum value in the tree: " << maxNode->data << endl;
+    }
+    else {
+        cout << "Tree is empty" << endl;
+    }
+
+    
+    int key_value;
+    cout << "Enter the value to find its successor: "; // Finding successor
+    cin >> key_value;
+
+    Node* successorNode = findSuccessor(root, key_value);
+    if (successorNode != NULL) {
+        cout << "Successor of " << key_value << " is " << successorNode->data << endl;
+    }
+    else {
+        cout << "No successor found for " << key_value << endl;
+    }
+    
+
+    int keyToFindPredecessor;
+    cout << "Enter the value to find its predecessor: "; // Finding predecessor
+    cin >> keyToFindPredecessor;
+
+    Node* predecessorNode = findPredecessor(root, keyToFindPredecessor);
+    if (predecessorNode != NULL) {
+        cout << "Predecessor of " << keyToFindPredecessor << " is " << predecessorNode->data << endl;
+    }
+    else {
+        cout << "No predecessor found for " << keyToFindPredecessor << endl;
+    }
+
 
     return 0;
 }
