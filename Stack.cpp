cc732a53bd6d630b7a011854f991eb8ebfaf905a
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-05-09 05:04:30 +0600

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,95 +1,65 @@
-// stack data structure by Class
-
 #include <bits/stdc++.h>
 using namespace std;
 
-class Stack {
-  private:
-    int top;
-  int arr[5];
+#define MAX_SIZE 5
 
-  public:
-    Stack() {
-      top = -1;
-      for (int i = 0; i < 5; i++) {
-        arr[i] = 0;
-      }
+int top = -1;
+int arr[MAX_SIZE];
+
+void push(int val) {
+    if (top == MAX_SIZE-1) {
+        cout << "stack overflow" << endl;
     }
-
-  bool isEmpty() {
-    if (top == -1)
-      return true;
-    else
-      return false;
-  }
-  bool isFull() {
-    if (top == 4)
-      return true;
-    else
-      return false;
-  }
-
-  void push(int val) {
-    if (isFull()) {
-      cout << "stack overflow" << endl;
-    } else {
-      top++; // 1
-      arr[top] = val;
+    else {
+        top++;
+        arr[top] = val;
     }
-  }
+}
 
-  int pop() {
-    if (isEmpty()) {
-      cout << "stack underflow" << endl;
-      return 0;
-    } else {
-      int popValue = arr[top];
-      arr[top] = 0;
-      top--;
-      return popValue;
+int pop() {
+    if (top == -1) {
+        cout << "stack underflow" << endl;
+        return 0;
     }
-  }
-
-  int count() {
-    return (top + 1);
-  }
-
-  int peek(int pos) {
-    if (isEmpty()) {
-      cout << "stack underflow" << endl;
-      return 0;
-    } else {
-      return arr[pos];
+    else {
+        int popValue = arr[top];
+        arr[top] = 0;
+        top--;
+        return popValue;
     }
-  }
+}
 
-  void change(int pos, int val) {
-    arr[pos] = val;
-    cout << "value changed at location " << pos << endl;
-  }
+int peek(int pos) {
+    if (top == -1) {
+        cout << "stack underflow" << endl;
+        return 0;
+    }
+    else {
+        return arr[pos];
+    }
+}
 
-  void display() {
+void display() {
     cout << "All values in the Stack are " << endl;
-    for (int i = 4; i >= 0; i--) {
-      cout << arr[i] << endl;
+
+    for (int i = top; i >= 0; i--) {
+        cout << arr[i] << endl;
     }
-  }
-};
+}
 
 int main() {
-  Stack s1;
-  s1.push(1);
-  s1.push(2);
-  s1.push(3);
-  s1.push(4);
-  s1.push(5);
-  s1.display();
+  push(1);
+  push(2);
+  push(3);
+  push(4);
+  push(5);
+  display();
 
-  s1.pop();
-  s1.display();
+  pop();
+  display();
 
-  s1.change(2, 6);
-  s1.display();
+  cout << "value at "<< 2 << " position is : " << peek(2) << endl<<endl;
 
- return 0;
+  return 0;
 }
+
