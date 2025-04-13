fa9724e5f63f327338511659b689b98b2a6db6aa
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2025-04-13 12:14:13 +0600

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,12 +1,10 @@
-// O(logn)
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[10] = {0, 5, 2, 7, 4, 3, 9, 8, 11, 2};
-int tree[30];
+vector<int> arr;
+vector<int> tree;
 
-void init (int node, int b, int e) {
+void init(int node, int b, int e) {
     if (b == e) {
         tree[node] = arr[b];
         return;
@@ -14,75 +12,72 @@ void init (int node, int b, int e) {
 
     int mid = (b + e) / 2;
     int left = node * 2;
-    int right = (node * 2) + 1;
+    int right = node * 2 + 1;
 
     init(left, b, mid);
-    init(right, mid+1, e);
+    init(right, mid + 1, e);
 
     tree[node] = tree[left] + tree[right];
 }
 
-int sum (int node, int b, int e, int i, int j) {
-    if (j < b || i > e) {
-        return 0;
-    }
-
-    if (i <= b && j >= e) {
-        return tree[node];
-    }
+int query(int node, int b, int e, int i, int j) {
+    if (j < b || i > e) return 0;
+    if (i <= b && e <= j) return tree[node];
 
     int mid = (b + e) / 2;
-    int left = 2 * node;
-    int right = (2 * node) + 1;
+    int left = node * 2;
+    int right = node * 2 + 1;
 
-    int l = sum(left, b, mid, i, j);
-    int r = sum(right, mid+1, e, i, j);
+    int l = query(left, b, mid, i, j);
+    int r = query(right, mid + 1, e, i, j);
 
     return l + r;
 }
 
-void update (int node, int b, int e, int i, int j, int value) {
-    if (j < b || i > e) {
-        return;
-    }
+void update(int node, int b, int e, int index, int value) {
+    if (index < b || index > e) return;
 
-    if (i <= b && j >= e) {
+    if (b == e) {
         tree[node] = value;
         return;
     }
 
     int mid = (b + e) / 2;
     int left = node * 2;
-    int right = (node * 2) + 1;
+    int right = node * 2 + 1;
 
-    update (left, b, mid, i, j, value);
-    update (right, mid+1, e, i, j, value);
+    update(left, b, mid, index, value);
+    update(right, mid + 1, e, index, value);
 
     tree[node] = tree[left] + tree[right];
 }
 
 int main() {
-    init (1, 1, 9);
+    int n, q;
+    cin >> n >> q;
 
-    for (int i = 0; i < 30; i++) {
-        cout << "node: " << i << " value: " << tree[i] << endl;
+    arr.resize(n + 1);
+    tree.resize(4 * n);
+
+    for (int i = 1; i <= n; i++) { // 3 2 4 5 1 1 5 3
+        cin >> arr[i];
     }
 
-    int q;
-    cin >> q;
+    init(1, 1, n);
 
     while (q--) {
-        int type;
+        string type;
         cin >> type;
-        if(type == 1) {
-            int i, j;
-            cin >> i >> j;
-
-            cout << sum (1, 1, 9, i, j) << endl;
-        } else {
-            int i, value;
-            cin >> i >> value;
-            update (1, 1, 9, i, i, value);
+        if (type == "sum") {
+            int l, r;
+            cin >> l >> r;
+            cout << query(1, 1, n, l, r) << endl;
+        } 
+        else if (type == "update") {
+            int index, value;
+            cin >> index >> value;
+            arr[index] = value;
+            update(1, 1, n, index, value);
         }
     }
 
